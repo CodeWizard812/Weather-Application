aac9d1e613bbe345e49c7e3bd3261c1abff3de6b
CodeWizard812 <shirish.professional@gmail.com>
2024-12-29 12:37:34 +0530

    Complete API Integration and Basic Web Implementation

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const APIKey = process.env.yourAPIKey;
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//Static files stored in public folder
+app.use(express.static("public"));
+
+
+app.get("/", async (req, res) =>{
+  try{
+    const response = await axios.get("https://api.weatherapi.com/v1/forecast.json?", {
+      params: {
+        key: APIKey,
+        q: "20.045865,78.939981",
+        days: 10,
+        aqi: "yes",
+        alerts:"yes",
+      },
+    });
+    res.render("index.ejs",{
+      data: response.data,
+      c : response.data.current,
+      f : response.data.forecast,
+    })
+  }catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
+})
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
